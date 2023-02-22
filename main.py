d6aec1043d02c1a50bfd484576368d590fd8a220
Thiago Rodrigues <trod.dev@gmail.com>
2023-02-22 18:31:28 -0300

    Python first step

---
+++ main.py
@@ -0,0 +1,14 @@
+# FIRST EXERCISE
+x = 5
+# print(dir(int))
+# print(help(int))
+# print(x)
+# def multiply(number):
+#    return number * x
+# b = multiply(10)
+# print(b)
+
+a = ['U'] + ['RN']
+len(a)
+b = ['4'] * 4
+len(b)
